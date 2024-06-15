4795be68055c173d918525c0979dc9cc97f91458
francoismk <francois.szymczak@gmail.com>
2024-06-15 15:11:55 +0200

    initial commit

---
+++ sample-backend/src/app.service.ts
@@ -0,0 +1,20 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateUserRequest } from './create-user-request.dto';
+import { ClientProxy } from '@nestjs/microservices';
+import { createUserEvent } from './create-user.event';
+
+@Injectable()
+export class AppService {
+  private readonly users: any[] = []
+
+  constructor(@Inject('COMMUNICATION') private readonly communicationClient: ClientProxy) {}
+
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  createUser(createUserRequest: CreateUserRequest) {
+    this.users.push(createUserRequest)
+    this.communicationClient.emit('user_created', new createUserEvent(createUserRequest.email))
+  }
+}
